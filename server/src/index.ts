f428d95844e51b3e9777ad32438730e5205959b8
Dilyana Musievska <dilyana.musievska@gmail.com>
2023-03-05 20:24:05 +0200

    added prettier-eslint configs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,17 @@
-import express, { Express, Request, Response } from 'express';
-import * as dotenv from 'dotenv';
+import express, { type Express, type Request, type Response } from 'express'
+import * as dotenv from 'dotenv'
+// import * as cookie-parser from 'cookie-parser';
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT;
+const app: Express = express()
+const port = process.env.PORT
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hi!');
-});
+  res.send('Hi!')
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: running on ${port}`);
-});
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  console.log(`⚡️[server]: running on ${port}`)
+})
