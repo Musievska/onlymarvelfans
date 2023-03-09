22ae2d33626027224d22c06bda8e6e6111ad4753
Dilyana Musievska <dilyana.musievska@gmail.com>
2023-03-09 15:43:25 +0200

    added security layers
    as helmet and cors. need to add TLS certificate!!!

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,14 @@
 import express, { type Express, type Request, type Response } from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
 
 const app: Express = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+app.use(helmet())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hi!')
