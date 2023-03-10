829e0a424a2a6c767975035b83212f88518fecd2
Dilyana Musievska <dilyana.musievska@gmail.com>
2023-03-10 14:09:37 +0200

    Update corsOptions.config.ts

--- server/src/config/corsOptions.config.ts
+++ server/src/config/corsOptions.config.ts
@@ -7,7 +7,8 @@ export const corsOptions = {
     origin: string,
     callback: (arg0: Error | null, arg1?: boolean | undefined) => void,
   ): void => {
-    if (allowOrigin.includes(origin) || origin === '') {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (allowOrigin.indexOf(origin) || origin === '') {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
